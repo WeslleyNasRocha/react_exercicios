291a3b5acff9d87ed38a823e9e7519c03163e139
Weslley Rocha <weslleytato.12@gmail.com>
2017-10-18 17:01:46 -0200

    primeiro componente

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(<h1>Olá React</h1>, document.getElementById('app'));
+import Component from './componente';
+
+ReactDOM.render(<Component />, document.getElementById('app'));
