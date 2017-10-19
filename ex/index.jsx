6a6e1e738771b8f06db9c3e2db2edcd2e598559c
Weslley Rocha <weslleytato.12@gmail.com>
2017-10-19 15:01:57 -0200

    Class component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Family from './family';
-import Member from './member';
+import ClassComponent from './classComponent';
 
 ReactDOM.render(
-  <Family lastName="Rocha">
-    <Member name="Weslley" />
-    <Member name="Rafael" />
-    <Member name="Julia" />
-  </Family>,
+  <ClassComponent value="Hello, from a class" />,
   document.getElementById('app')
 );
