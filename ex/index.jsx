538589a4e8d8d52da5f8141654ba42de7b3fcdd4
Weslley Rocha <weslleytato.12@gmail.com>
2017-10-19 14:53:27 -0200

    family

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import SilvaFamily from './silvaFamily';
+import Family from './family';
+import Member from './member';
 
-ReactDOM.render(<SilvaFamily />, document.getElementById('app'));
+ReactDOM.render(
+  <Family lastName="Rocha">
+    <Member name="Weslley" />
+    <Member name="Rafael" />
+    <Member name="Julia" />
+  </Family>,
+  document.getElementById('app')
+);
